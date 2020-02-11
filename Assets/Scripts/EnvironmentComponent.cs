2a2b20ebf7be7eed4e3b1cc789b12ded1d47829b
Daniel <dan1510123@gmail.com>
2020-02-10 21:49:20 -0500

    Pulled Juice's code for EnvironmentComponents Merged Juice's branch and EnvironmentComponents

---
+++ Assets/Scripts/EnvironmentComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace EnvironmentComponents
+{
+    // Designating whether or not player collides
+    public enum WallComponent
+    {
+        Collideable = 0, //player can't walk through
+        Scenery = 1     //player can walk through
+    }
+
+    public struct TranslationComponent : IComponentData
+    {
+        public Transform startingPosition; //Where it starts
+        public float3 movement;            //movespeeds
+    }
+
+    public struct CollisionComponent : IComponentData
+    {
+        //TODO
+    }
+}
