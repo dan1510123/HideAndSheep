2562e1e2a43a22a79956014b701c2824cdab042f
avl22 <avl22@case.edu>
2020-02-11 10:44:35 -0500

    added enemy

---
+++ Assets/Scripts/EnemyEntity.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Rendering;
+
+public class EnemyEntity : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    private void Start()
+    {
+        EntityManager entityManager = World.Active.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(Collider2D),
+            typeof(WeaponComponent),
+            typeof(StatsComponent),
+            typeof(EnemyComponent)
+        );
+
+        Entity e = entityManager.CreateEntity(entityArchetype);
+
+        entityManager.SetComponentData(e, new MovingComponent
+        {
+            currMovementDirection = 0
+        });
+        entityManager.SetComponentData(e, new Translation
+        {
+            Value = new float3(3, 3, 0)
+        });
+
+        entityManager.SetSharedComponentData(e, new RenderMesh
+        {
+            mesh = mesh,
+            material = material
+
+        });
+
+    }
+}
