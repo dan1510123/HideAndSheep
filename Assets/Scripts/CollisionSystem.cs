a8521cbd56ae45d4138e147c4e4bbbe6041579e0
Justin Shearson <justin.shearson@case.edu>
2020-02-18 12:08:09 -0500

    Renamed StaticStuff to GlobalObjects and added CollisionSystem class with checkCollision method stub.

---
+++ Assets/Scripts/CollisionSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+public class CollisionSystem : ComponentSystem
+{
+    public void checkColision<T, K>(ref T collider1, ref K collider2) 
+        where T: IComponentData 
+        where K : IComponentData
+    {
+
+    }
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
