128c571e143d3e066d48dce49623955f0df73a96
Zasilus <asg101@case.edu>
2020-03-23 13:08:37 -0400

    Forgot to add the files before committing these are the new files. HudSystem interacts with the ECS system whereas UIMenu is a more monobehaviour class that mainly is for interfacing
    Signed-off-by: Zasilus <asg101@case.edu>

---
+++ Assets/Scripts/HUD/UIMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            onClick();
+        }
+    }
+
+    public void onClick()
+    {
+        if (Time.timeScale == 1)
+        {
+            Debug.Log("FREEZE");
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0)
+        {
+            Debug.Log("EVERYBODY CLAP YOUR HANDS");
+            Time.timeScale = 1;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Systems/HudSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Components;
+using EnvironmentComponents;
+using EntityComponents;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudSystem : ComponentSystem
+{
+    // Start is called before the first frame update
+    public Text healthText;
+    public Image healthBar;
+    public Text statText;
+    void Start()
+    {
+    }
+    protected override void OnUpdate()
+    {
+        healthText = GameObject.Find("HealthText").GetComponent<Text>();
+        healthBar = GameObject.Find("Image").GetComponent<Image>();
+        statText = GameObject.Find("StatText").GetComponent<Text>();
+        //This Grabs everyone with these components
+        //AKA the player
+        Entities.ForEach((Entity e,
+            ref PlayerComponent playerComponent,
+            ref MovementComponent movementComponent,
+            ref StatsComponent statsComponent,
+            ref VelocityComponent velocityComponent,
+            ref ColliderComponent colliderComponent) =>
+        {
+            //StatsComponent updateComponent = statsComponent;
+            //Entities.ForEach((Entity f,
+            //    ref HealthBarComponent healthBarComponent) =>
+            //{
+            //    healthBarComponent.health = updateComponent.health;
+            //    healthBarComponent.attack = updateComponent.attack;
+            //    healthBarComponent.attackSpeed = updateComponent.attackSpeed;
+            //    healthBarComponent.moveSpeed = updateComponent.moveSpeed;
+            //});
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                statsComponent.health--;
+                Debug.Log("The Current Health is decremented to " + statsComponent.health);
+
+            }
+            healthText.text = "Current Health: " + statsComponent.health;
+            healthBar.rectTransform.sizeDelta = new Vector2(statsComponent.health * 40, 20);
+            statText.text = "Attack Damage: " + statsComponent.attack + "\nAttack Speed: " + statsComponent.attackSpeed + "\nMove Speed: " + statsComponent.moveSpeed;
+
+        });
+
+    }
+}
