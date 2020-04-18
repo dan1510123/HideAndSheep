896a63baa4202c59d0db919dba40cacfe06cb73b
Justin Shearson <justinshearson@gmail.com>
2020-04-18 15:27:00 -0400

    Added background image to menu as well as updating visual components of sliders

--- Assets/Scripts/HUD/MainMenuController.cs
+++ Assets/Scripts/HUD/MainMenuController.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Components;
+using Unity.Entities;
+using Unity;
 public class MainMenuController : MonoBehaviour
 {
 
     [SerializeField] private GameObject buttons;
     [SerializeField] private GameObject volumeSliders;
+    [SerializeField] private Image background;
 
-  
+    private Vector2 backgroundDelta = new Vector2(300, 400);
+    private Vector3 backgroundOffset = new Vector2(0, 75);
     //Enables the buttons in the options menu
     public void GoToOptions()
     {
         buttons.SetActive(false);
         volumeSliders.gameObject.SetActive(true);
-
+        background.rectTransform.sizeDelta += backgroundDelta;
+        background.rectTransform.localPosition -= backgroundOffset;
     }
 
     //Goes back to the main menu screen
@@ -22,6 +28,8 @@ public class MainMenuController : MonoBehaviour
     {
         buttons.SetActive(true);
         volumeSliders.SetActive(false);
+        background.rectTransform.sizeDelta += -backgroundDelta;
+        background.rectTransform.localPosition += backgroundOffset;
 
     }
 
--- Assets/Scripts/Systems/HudSystem.cs
+++ Assets/Scripts/Systems/HudSystem.cs
@@ -6,50 +6,60 @@ using EntityComponents;
 using Unity.Entities;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class HudSystem : ComponentSystem
 {
     // Start is called before the first frame update
     public Text healthText;
     public Image healthBar;
     public Text statText;
-    void Start()
-    {
-    }
+    private bool sceneLoaded = false;
+    UnityEngine.SceneManagement.Scene currentScene;
     protected override void OnUpdate()
     {
-        healthText = GameObject.Find("HealthText").GetComponent<Text>();
-        healthBar = GameObject.Find("Image").GetComponent<Image>();
-        statText = GameObject.Find("StatText").GetComponent<Text>();
-        //This Grabs everyone with these components
-        //AKA the player
-        Entities.ForEach((Entity e,
-            ref PlayerComponent playerComponent,
-            ref MovementComponent movementComponent,
-            ref StatsComponent statsComponent,
-            ref VelocityComponent velocityComponent,
-            ref ColliderComponent colliderComponent) =>
+        if (!sceneLoaded)
         {
-            //StatsComponent updateComponent = statsComponent;
-            //Entities.ForEach((Entity f,
-            //    ref HealthBarComponent healthBarComponent) =>
-            //{
-            //    healthBarComponent.health = updateComponent.health;
-            //    healthBarComponent.attack = updateComponent.attack;
-            //    healthBarComponent.attackSpeed = updateComponent.attackSpeed;
-            //    healthBarComponent.moveSpeed = updateComponent.moveSpeed;
-            //});
-            if (Input.GetKeyDown(KeyCode.M))
+            currentScene = SceneManager.GetActiveScene();
+            if(currentScene.name == "SampleScene")
             {
-                statsComponent.health--;
-                Debug.Log("The Current Health is decremented to " + statsComponent.health);
-
+                sceneLoaded = true;
             }
-            healthText.text = "Current Health: " + statsComponent.health;
-            healthBar.rectTransform.sizeDelta = new Vector2(statsComponent.health * 40, 20);
-            statText.text = "Attack Damage: " + statsComponent.attack + "\nAttack Speed: " + statsComponent.attackSpeed + "\nMove Speed: " + statsComponent.moveSpeed;
+        }
+        else
+        {
+            healthText = GameObject.Find("HealthText").GetComponent<Text>();
+            healthBar = GameObject.Find("Image").GetComponent<Image>();
+            statText = GameObject.Find("StatText").GetComponent<Text>();
+            //This Grabs everyone with these components
+            //AKA the player
+            Entities.ForEach((Entity e,
+                ref PlayerComponent playerComponent,
+                ref MovementComponent movementComponent,
+                ref StatsComponent statsComponent,
+                ref VelocityComponent velocityComponent,
+                ref ColliderComponent colliderComponent) =>
+            {
+                //StatsComponent updateComponent = statsComponent;
+                //Entities.ForEach((Entity f,
+                //    ref HealthBarComponent healthBarComponent) =>
+                //{
+                //    healthBarComponent.health = updateComponent.health;
+                //    healthBarComponent.attack = updateComponent.attack;
+                //    healthBarComponent.attackSpeed = updateComponent.attackSpeed;
+                //    healthBarComponent.moveSpeed = updateComponent.moveSpeed;
+                //});
+                if (Input.GetKeyDown(KeyCode.M))
+                {
+                    statsComponent.health--;
+                    Debug.Log("The Current Health is decremented to " + statsComponent.health);
 
-        });
+                }
+                healthText.text = "Current Health: " + statsComponent.health;
+                healthBar.rectTransform.sizeDelta = new Vector2(statsComponent.health * 40, 20);
+                statText.text = "Attack Damage: " + statsComponent.attack + "\nAttack Speed: " + statsComponent.attackSpeed + "\nMove Speed: " + statsComponent.moveSpeed;
 
+            });
+
+        }
     }
 }
