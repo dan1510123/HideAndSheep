19d5ff93dc9a90080c4f8711406c568f21e169a3
Justin Shearson <justinshearson@gmail.com>
2020-04-22 18:08:10 -0400

    Changed the main menu to have a host and join option (incomplete) and added IpRequestManager

---
+++ Assets/Scripts/HUD/IpRequestManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.HUD
+{
+    public class IpRequestManager : MonoBehaviour
+    {
+        public string HostIp;
+        public bool requestFinished;
+
+        private IEnumerator SendRequest(string url)
+        {
+            UnityWebRequest request = UnityWebRequest.Get(url);
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+                HostIp = "0.0.0.0";
+                requestFinished = true;
+            }
+            else
+            {
+                HostIp = request.downloadHandler.text;
+                requestFinished = true;
+            }
+            yield return null;
+        }
+
+        public IEnumerator GetHostIP(string url)
+        {
+            if (!requestFinished)
+            {
+                StartCoroutine(SendRequest(url));
+
+            }
+            yield return null;
+
+        }
+    }
+
+}
