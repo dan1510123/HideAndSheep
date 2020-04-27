74388c9574a9f9119c3480cab6926b8f5c01ab77
Justin Shearson <justinshearson@gmail.com>
2020-04-26 20:02:34 -0400

    Changed mainmenu and removed unused functions in MainMenuController

--- Assets/Scripts/HUD/MainMenuController.cs
+++ Assets/Scripts/HUD/MainMenuController.cs
@@ -14,25 +14,11 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject buttons;
     [SerializeField] private GameObject volumeSliders;
     [SerializeField] private Image background;
-    [SerializeField] private GameObject playOptions;
-    [SerializeField] private GameObject HostOptions;
-    [SerializeField] private GameObject JoinOptions;
-    private readonly string getURL = "https://api.ipify.org";
-    private string publicIp;
     private Vector2 backgroundDelta = new Vector2(300, 400);
     private Vector3 backgroundOffset = new Vector2(0, 75);
     private IpRequestManager requestManager;
-    private Text hostIpText;
 
-    public void Start()
-    {
-        requestManager = transform.gameObject.AddComponent<IpRequestManager>();
-        hostIpText = HostOptions.GetComponentInChildren<Text>();
-        StartCoroutine(GetHostIp());
 
-    }
-
-    #region GoTos
     //Enables the buttons in the options menu
     public void GoToOptions()
     {
@@ -42,29 +28,8 @@ public class MainMenuController : MonoBehaviour
         background.rectTransform.localPosition -= backgroundOffset;
     }
 
-    //Go to the singleplayer & multiplayer menu
-    public void GoToPlayOptions()
-    {
-        playOptions.SetActive(true);
-        buttons.SetActive(false);
-    }
 
-    public void GoToHostOptions()
-    {
-        background.rectTransform.sizeDelta += backgroundDelta * .83f;
-        playOptions.SetActive(false);
-        HostOptions.SetActive(true);
-    }
 
-    public void GotToJoinOptions()
-    {
-        background.rectTransform.sizeDelta += backgroundDelta * .83f;
-        playOptions.SetActive(false);
-        JoinOptions.SetActive(true);
-    }
-    #endregion
-
-    #region BackTos
     //Goes back to the main menu screen
     public void BackToMenuFromVolumeSliders()
     {
@@ -74,29 +39,6 @@ public class MainMenuController : MonoBehaviour
         background.rectTransform.localPosition += backgroundOffset;
 
     }
-
-    //Goes back to main menu from play options menu
-    public void BackToMenuFromPlayOptions()
-    {
-        buttons.SetActive(true);
-        playOptions.SetActive(false);
-    }
-
-    //Goes back to the play options menu
-    public void BackToPlayOptionsFromHost()
-    {
-        background.rectTransform.sizeDelta -= backgroundDelta * .83f;
-        playOptions.SetActive(true);
-        HostOptions.SetActive(false);
-    }
-
-    public void BackToPlayOptionsFromJoin()
-    {
-        background.rectTransform.sizeDelta -= backgroundDelta * .83f;
-        playOptions.SetActive(true);
-        JoinOptions.SetActive(false);
-    }
-    #endregion
 
     #region SoundOptions
     //Changes the game volume
@@ -119,46 +61,13 @@ public class MainMenuController : MonoBehaviour
 
     #endregion
 
-    #region PlayOptions
-    /// <summary>
-    /// Plays the game
-    /// </summary>
     public void PlayGame()
     {
         Application.LoadLevel(1);
     }
 
-    /// <summary>
-    /// Plays the game
-    /// </summary>
-    /// <param name="modifier">Value is 0 if host, 1 if client</param>
-    public void PlayGame(int modifier)
-    {
-        Application.LoadLevel(1);
-    }
-
-
     public void quitGame()
     {
         Application.Quit();
     }
-    #endregion
-
-    #region Helpers
-    private IEnumerator GetHostIp()
-    {
-        float loopTime = 0;
-        yield return StartCoroutine(requestManager.GetHostIp(getURL));
-
-        //while (!requestManager.requestFinished && loopTime < 2)
-        //{
-        //    loopTime += Time.deltaTime;
-        //}
-        //yield return new WaitForSeconds(.5f);
-        publicIp = requestManager.HostIp;
-
-        hostIpText.text = "Your public IP is " + publicIp + "\n Send this to your friend so they can join.";
-        yield return null;
-    }
-    #endregion
 }
